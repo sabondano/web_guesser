c1c49e73483f2f2044c93919d5a16d7a307609e9
Sebastian Abondano <sabondano1@gmail.com>
2015-06-30 15:14:06 -0600

    I4: Added colorizing the output extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,23 +4,23 @@ require 'sinatra/reloader'
 SECRET_NUMBER = rand(100)
 
 get '/' do
-  guess = params["guess"].to_i
-  message = check_guess(guess.to_i)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  guess = params["guess"]
+  message, background = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background => background}
 end
 
 def check_guess(guess)
-  if (guess + 5) < SECRET_NUMBER
-    "Way too low!"
-  elsif guess < SECRET_NUMBER
-    "Too Low!"
-  elsif (guess - 5) > SECRET_NUMBER
-    "Way too high!"
-  elsif guess > SECRET_NUMBER
-    "Too high!"
-  elsif guess == SECRET_NUMBER
-    "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
+  if guess == nil
+    ["Choose a number between 0 - 99!", "#FFFFFF"]
+  elsif (guess.to_i + 5) < SECRET_NUMBER
+    ["Way too low!", "#FF0000"]
+  elsif guess.to_i < SECRET_NUMBER
+    ["Too Low!", "#FF9999"]
+  elsif (guess.to_i - 5) > SECRET_NUMBER
+    ["Way too high!", "#FF0000"]
+  elsif guess.to_i > SECRET_NUMBER
+    ["Too high!", "#FF9999"]
+  elsif guess.to_i == SECRET_NUMBER
+    ["You got it right! The SECRET NUMBER is #{SECRET_NUMBER}", "#008000"]
   end
 end
-
-
